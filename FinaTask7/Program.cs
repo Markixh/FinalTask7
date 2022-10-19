6b0afdf84047a22a56c2a6a504b7614519a324ce
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-19 20:06:00 +0300

    Добавлены классы Доставки и Заказа

--- FinaTask7/Program.cs
+++ FinaTask7/Program.cs
@@ -6,5 +6,57 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        /// <summary>
+        /// Базовый класс доставки
+        /// </summary>
+        abstract class Delivery
+        {
+            public string Address;
+        }
+
+        /// <summary>
+        /// Доставка домой
+        /// </summary>
+        class HomeDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        /// <summary>
+        /// Доставка до автомата выдачи
+        /// </summary>
+        class PickPointDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        /// <summary>
+        /// Доставка в магазин
+        /// </summary>
+        class ShopDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        /// <summary>
+        /// Заказ
+        /// </summary>
+        /// <typeparam name="TDelivery"></typeparam>
+        class Order<TDelivery> where TDelivery : Delivery
+        {
+            public TDelivery Delivery;
+
+            public int Number;
+
+            public string Description;
+
+            public void DisplayAddress()
+            {
+                Console.WriteLine(Delivery.Address);
+            }
+
+            // ... Другие поля
+        }
     }
 }
