4aefd64ad576b3fc4c8fa19cab66db1c87232b49
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-20 21:21:21 +0300

    Revert "Revert "Добавлена библиотека клвссов""
    This reverts commit ade7106e49c54fec3d86673c4ed0abf1de3cfbb9.

---
+++ FinalTask7.Library/Delivery.cs
@@ -0,0 +1,34 @@
+﻿namespace FinalTask7.Library
+{
+    /// <summary>
+    /// Базовый класс доставки
+    /// </summary>
+    public abstract class Delivery
+    {
+        public string Address;
+    }
+
+    /// <summary>
+    /// Доставка домой
+    /// </summary>
+    public class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    /// <summary>
+    /// Доставка до автомата выдачи
+    /// </summary>
+    public class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    /// <summary>
+    /// Доставка в магазин
+    /// </summary>
+    public class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+}
---
+++ FinalTask7.Library/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalTask7.Library
+{
+    /// <summary>
+    /// Товар
+    /// </summary>
+    class Product
+    {
+        private string name;
+        private decimal price;
+
+        public string Name { get { return name; } }
+        public decimal Price { get { return price; } }
+    }
+}
